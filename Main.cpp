88b2fc3eda3c4f77932ed862216f9fea3fc68e01
Brandon Lingo <brandonlingo0@gmail.com>
2024-10-07 01:54:06 -0700

    multi threading for multi clients

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,19 @@
 #include <string>
 #include <WinSock2.h>
 #include "Headers/ServerFunctions.h"
+#include <thread>
+
+/*
+	Server Function Steps:
+
+	1. Initialize WSA - WSAStartup()
+	2. Create a socket - socket()
+	3. Bind the socket - bind()
+	4. Listen on the socket - listen()
+	5. Accept a connection - accept(), connect()
+	6. Receive and send data - recv(), send(), recvfrom(), sendto()
+	7. Disconnect - closesocket()
+*/
 
 using namespace std;
 
@@ -15,9 +28,21 @@ int main() {
 	socks = socket();
 	bindSocket(port,socks);
 	listen(socks);
-	acceptedSocket = acceptSocket(socks);
-	receiveData(acceptedSocket);
 
+	// multi threading to allow for multiple clients
+	while (true){
+		acceptedSocket = acceptSocket(socks);
+
+		if (acceptedSocket == INVALID_SOCKET) {
+			// break from while loop if the socket is invalid
+			break;
+		}
+
+		thread clientThread(receiveData, acceptedSocket);	// pass in recieve function and its SOCKET paremeter
+		clientThread.detach();		// allows the thread to run independent of the main thread
+	}
+
+	closesocket(socks);
 	WSACleanup();
 	return 0;
 }
